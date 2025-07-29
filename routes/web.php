ee22da5e6c756dd38de3a6023bbbb3762db3f034
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2025-07-30 00:35:48 +0600

    route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 use App\Http\Controllers\TaskController;
 
-// সব টাস্ক দেখানোর জন্য
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
-// নতুন টাস্ক সংরক্ষণের জন্য
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-
-// টাস্ক ডিলিট করার জন্য
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 
